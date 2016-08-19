5e71499635a8dc9ee0e700cfbc769ca897617ecd
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-19 12:39:01 +0300

    amend mix.exs

--- mix.exs
+++ mix.exs
@@ -7,26 +7,31 @@ defmodule PrometheusPhoenix.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps,
+     description: description,
+     package: package]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :prometheus]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp description do
+    """
+    Prometheus monitoring system client Phoenix instrumenter.
+    """
+  end
+
+  defp package do
+    [maintainers: ["Ilya Khaprov"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/deadtrickster/prometheus-phoenix",
+              "Phoenix Plugs" => "https://github.com/deadtrickster/prometheus-plugs",
+              "Ecto integration" => "https://github.com/deadtrickster/prometheus-ecto"}]
+  end
+
   defp deps do
-    []
+    [{:prometheus, "~> 2.0"},
+     {:phoenix, "~> 1.2"}]
   end
 end
